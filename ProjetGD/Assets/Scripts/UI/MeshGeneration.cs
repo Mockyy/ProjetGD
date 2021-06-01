906ccc2e578cc57e198ab8fa595bf42ab5172b6d
Mockyy <r.mock@ludus-academie.com>
2021-06-01 11:24:09 +0200

    Textures
    Ajout de textures trouvées sur l'Asset Store

--- ProjetGD/Assets/Scripts/UI/MeshGeneration.cs
+++ ProjetGD/Assets/Scripts/UI/MeshGeneration.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class MeshGeneration : MonoBehaviour
 {
     [SerializeField] private Material mat;
-    private float width = 1;
-    private float height = 1;
+    private float width = 0.2f;
+    private float height = 2;
 
     // Start is called before the first frame update
     void Start()
+    {
+        DrawArrow();
+    }
+
+    private void DrawArrow()
     {
         MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
         meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
@@ -18,20 +23,23 @@ public class MeshGeneration : MonoBehaviour
 
         Mesh mesh = new Mesh();
 
-        Vector3[] vertices = new Vector3[3]
+        Vector3[] vertices = new Vector3[7]
         {
-            new Vector3(-1, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 0, 0)
+            new Vector3(-width, 0, 0),
+            new Vector3(width, 0, 0),
+            new Vector3(width, height, 0),
+            new Vector3(1, height, 0),
+            new Vector3(0, 3, 0),
+            new Vector3(-1, height, 0),
+            new Vector3(-width, height, 0)
         };
         mesh.vertices = vertices;
 
-        int[] tris = new int[3]
+        int[] tris = new int[9]
         {
-            // lower left triangle
-            0, 1, 2,
-            // upper right triangle
-
+            1, 0, 6,
+            5, 4, 3,
+            1, 6, 2
         };
         mesh.triangles = tris;
 
